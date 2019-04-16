5263416ecfc7cd3365ce85897260150b10c09066
Bryan Baird <bryan@kerigan.com>
2019-04-16 09:56:32 -0400

    report revisions

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -2,33 +2,39 @@
 
 @section('content')
     {{-- $finalReport, $company --}}
-    <div class="wrapper py-3">
+    <div class="wrapper py-3 pt-5">
         <div class="container">
             <div class="row align-items-center justify-content-between ">
-                <div class="col-md-5 col-lg-4 col-xl-3 p-3 text-center">
-                    <img src="/img/kma-logo.png" alt="KMA" class="img-fluid" width="250">
+                
+                <div class="col-lg-4 col-lg-4 col-xl-3 text-lg-left pb-3 text-center">
+                    <img src="/img/kma-logo.png" alt="KMA" class="img-fluid" width="170">
                 </div>
-                <div class="col-auto p-3 pt-4 flex-grow-1">
-                    <p class="text-center text-md-right text-primary display-2"><strong>{{ $company->name }}</strong></p>
-                    <p class="text-center text-md-right text-secondary">{{ Carbon\Carbon::create($finalReport->year, $finalReport->month, 1)->format('F Y') }} Website Statistics Report</p>
+
+                <div class="col-md-6 col-lg-4 p-3 pt-1">
+                    <p class="text-center text-secondary display-3"><strong>{{ $company->name }}</strong><br><a target="_blank" href="{{ $company->url }}" class="display-4">{{ $company->url }}</a></p>
                 </div>
+                
+                <div class="col-md-6 col-lg-4 col-xl-3 p-3 pt-1 flex-grow-1">
+                    <p class="text-center text-lg-right text-primary display-3"><strong>{{ Carbon\Carbon::create($finalReport->year, $finalReport->month, 1)->format('F Y') }}</strong><br>Website Statistics Report</p>
+                </div>
+
             </div>
             <div class="row mt-2">
                 <div class="col p-2 text-center bg-primary text-white">
-                    <p class="display-1">Audience Overview</p>
-                    <p class="display-4">{{ Carbon\Carbon::create($finalReport->year, $finalReport->month, 1)->format('F Y') }}
+                    <p class="display-1">Google Analytics</p>
+                    <p class="display-4"><strong>{{ Carbon\Carbon::create($finalReport->year, $finalReport->month, 1)->format('F Y') }}
                         vs
                         @if($finalReport->comparedWithLastMonth)
                             {{ Carbon\Carbon::create($finalReport->year, $finalReport->month - 1, 1)->format('F Y') }}
                         @else
                             {{ Carbon\Carbon::create($finalReport->year - 1, $finalReport->month, 1)->format('F Y') }}
                         @endif
-                    </p>
+                    </strong></p>
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-6 col-md-4 col-xl-2 text-center pt-4">
-                    <p class="display-3 attribute-header">Average Daily Sessions</p>
+                    <p class="display-3 attribute-header">Average Sessions Per Day</p>
                     <hr>
                     @php
                         if($finalReport->percent_change_sessions < 0){
@@ -152,8 +158,7 @@
                         {{ $finalReport->previous_bounce_rate }}%
                     </p>
                     <p class="description">
-                        The percentage of single-page visits (i.e. visits in which the person left your site from
-                        the entrance page without interacting with the page).
+                        The percentage of single-page sessions in which there was no interaction with the page.
                     </p>
                 </div>
             </div>
@@ -163,35 +168,41 @@
                     <div class="device-images row">
                         <div class="col-md-4 py-2 text-center">
                             <img src="/img/desktop.png" alt="desktop" class="device img img-fluid">
-                            <p class="text-center desktop">desktop</p>
-                            <hr>
+                            <p class="text-center pt-2">desktop / laptop</p>
                             <p class="big-number">{{$finalReport->desktop_percentage}}%</p>
                         </div>
                         <div class="col-md-4 py-2 text-center">
                             <img src="/img/phone.png" alt="desktop" class="device img img-fluid phone">
-                            <p class="text-center phone-text">mobile</p>
-                            <hr>
+                            <p class="text-center pt-2">smartphone</p>
                             <p class="big-number">{{$finalReport->mobile_percentage}}%</p>
                         </div>
                         <div class="col-md-4 py-2 text-center">
                             <img src="/img/tablet.png" alt="desktop" class="device img img-fluid tablet">
-                            <p class="text-center tablet-text">tablet</p>
-                            <hr>
+                            <p class="text-center pt-2">tablet</p>
                             <p class="big-number">{{$finalReport->tablet_percentage}}%</p>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
-                    <div class="new-vs-returning">
-                        <div class="returning col-md-6">
-                            <p class="big-number visitors">{{ $finalReport->returning_visitors }}%</p>
-                            <hr>
-                            <p class="compare-header text-center visitor-type">Returning Visitors</p>
+                    <div class="new-vs-returning-visitors d-flex flex-column">
+                        
+                        <div class="row h-50">
+                            <div class="col-12 pb-4">
+                                <h2 class="p-2 text-center bg-primary text-white">New vs Returning Visitors</h2>
+                            </div>
+                            <div class="new col-6">
+                                <p class="big-number visitors">{{ $finalReport->new_visitors }}%</p>
+                                <p class="compare-header text-center visitor-type">New Visitors</p>
+                            </div>
+                            <div class="returning col-6">
+                                <p class="big-number visitors">{{ $finalReport->returning_visitors }}%</p>
+                                <p class="compare-header text-center visitor-type">Returning Visitors</p>
+                            </div>
+                            
                         </div>
-                        <div class="new col-md-6">
-                            <p class="big-number visitors">{{ $finalReport->new_visitors }}%</p>
-                            <hr>
-                            <p class="compare-header text-center visitor-type">New Visitors</p>
+                        <div class="d-flex h-100 ">
+                            <div class="bg-secondary py-2" style="width:{{ $finalReport->new_visitors }}% !important; transform: skew(10deg);">&nbsp;</div>
+                            <div class="bg-info py-2" style="width:{{ $finalReport->returning_visitors }}% !important; transform: skew(10deg);">&nbsp;</div>
                         </div>
                     </div>
                 </div>
@@ -222,7 +233,7 @@
                 <div class="col-md-6 text-center no-gutter d-flex flex-wrap align-items-center traffic-source">
                     <span class="col-6 col-xl-4 text-right  display-3 px-2">Social: </span>
                     <span class="col-6 col-xl-3 display-1 text-left text-xl-center text-secondary">{{ number_format($finalReport->social, 1, '.', ',') }}%</span>
-                    <span class="text-left col-xl-5 text-center text-xl-left p-2 pt-0 pt-xl-2 flex-grow d-flex align-items-start"><small class="w-100">Users who click to your website from a social media page or post.</small></span>
+                    <span class="text-left col-xl-5 text-center text-xl-left p-2 pt-0 pt-xl-2 flex-grow d-flex align-items-start"><small class="w-100">Users who click to your website from a social media page, post, or ad.</small></span>
                 </div>
             </div>
             <div class="row">
@@ -232,7 +243,7 @@
                     <span class="text-left col-xl-5 text-center text-xl-left p-2 pt-0 pt-xl-2 flex-grow d-flex align-items-start"><small class="w-100">Users that type your domain name into their address bar, or have your site saved in their favorites</small></span>
                 </div>
                 <div class="col-md-6 text-center bg-light no-gutter d-flex flex-wrap align-items-center traffic-source">
-                    <span class="col-6 col-xl-4 text-right  display-3 px-2">Paid Search: </span>
+                    <span class="col-6 col-xl-4 text-right  display-3 px-2">Paid Promotion: </span>
                     <span class="col-6 col-xl-3 display-1 text-left text-xl-center text-secondary">{{ number_format($finalReport->paid_search, 1, '.', ',') }}%</span>
                     <span class="text-left col-xl-5 text-center text-xl-left p-2 pt-0 pt-xl-2 flex-grow d-flex align-items-start"><small class="w-100">Users who click to your website from a sponsored listing or display ad.</small></span>
                 </div>
