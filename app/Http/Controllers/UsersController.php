da346cd43cfc8389e0d57f9b22c8df805d0e0f0d
Bryan Baird <bryan@kerigan.com>
2020-07-10 16:38:22 -0400

    bug fix

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,7 +18,7 @@ class UsersController extends Controller
 
     public function show()
     {
-        $users = User::get();
+        $users = User::orderby('name')->get();
         return view('users.show', compact('users'));
     }
 
